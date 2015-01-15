c522254b0e42131b127133a08a0224e89f31d81b
Manuel Salazar <msalazar_@hotmail.com>
2015-01-15 09:34:14 -0500

    'ruta_cambiada'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,42 @@ class UsersController < ApplicationController
 		@usuarios = User.all
 	end
 
+	def new
+		@usuario = User.new
+	end
+
+	def create
+		@usuario = User.new(user_params)
+		if @usuario.save
+			redirect_to user_url(@usuario.id)
+		else
+			render action: 'new'
+		end
+	end
+
 	def show
 		@usuario = User.find(params[:id])
+	end
+
+	def edit
+		@usuario = User.find(params[:id])
+	end
+
+	def update
+		@usuario = User.find(params[:id])
+		if @usuario.update_attributes(user_params)
+			redirect_to user_url(@usuario.id)
+		else
+			render action: 'edit'
+		end
 	end
 
 	def destroy
 		User.find(params[:id]).destroy
 		redirect_to users_url
+	end
+
+	def user_params
+		params.require(:user).permit(:nombre, :email)
 	end
 end
